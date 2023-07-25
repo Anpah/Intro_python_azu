68e9ab1c5533f8d00c8e4584c9562d420f28d2a9
ampah_nathaniel <ampahnathaniel@gmial.com>
2023-07-25 23:34:22 +0000

    change23

---
+++ p.py
@@ -0,0 +1,5 @@
+# work will commence soon
+fruits_typpes = ("Mango", "Watermelon")
+Vegetables_types = ("Tomatoes", "Garden Eggs")
+#print(Vegetables_types[1]) ("Pepper")
+print(Vegetables_types + fruits_typpes)
